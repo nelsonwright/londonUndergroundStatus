6a518411c80b83f0badb89c6c451307e0bd55824
Nelson Wright <me@nelsonwright.co.uk>
2020-09-08 16:10:52 +0100

    Treat the end of the weekend as just before midnight on Sunday

--- app/src/main/java/uk/co/nelsonwright/londonundergroundstatus/shared/CalendarUtils.kt
+++ app/src/main/java/uk/co/nelsonwright/londonundergroundstatus/shared/CalendarUtils.kt
@@ -1,29 +1,37 @@
 package uk.co.nelsonwright.londonundergroundstatus.shared
 
+
 import java.text.SimpleDateFormat
 import java.util.*
 
 class CalendarUtils(private val timeHelper: TimeHelper) {
 
     fun getWeekendDates(): Pair<String, String> {
-        val yearMonthDayFormat = SimpleDateFormat("yyyy-MM-dd")
-        val thisSaturday = calculateThisSaturday()
+        val yearMonthDayFormat = SimpleDateFormat("yyyy-MM-dd", Locale.UK)
 
-        val saturdayDateString = yearMonthDayFormat.format(thisSaturday.time)
-        thisSaturday.add(Calendar.DATE, 1)
-        val sundayDateString = yearMonthDayFormat.format(thisSaturday.time)
-        return Pair(saturdayDateString, sundayDateString)
+        val uTCDateTimeFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.UK)
+        val rollingDate = calculateThisSaturday()
+
+        val saturdayDateString = yearMonthDayFormat.format(rollingDate.time)
+
+        // treat the end of the weekend as just before midnight on Sunday...
+        rollingDate.add(Calendar.DATE, 1)
+        rollingDate.set(Calendar.HOUR_OF_DAY, 23)
+        rollingDate.set(Calendar.MINUTE, 59)
+
+        val earlyMondayMorningDateTimeString = uTCDateTimeFormat.format(rollingDate.time)
+        return Pair(saturdayDateString, earlyMondayMorningDateTimeString)
     }
 
     fun getFormattedSaturdayDate(): String {
         // e.g. 2 May or 29 Apr . . .
-        val dateMonthFormat = SimpleDateFormat("d MMM")
+        val dateMonthFormat = SimpleDateFormat("d MMM", Locale.getDefault())
         return dateMonthFormat.format(calculateThisSaturday().time)
     }
 
     fun getFormattedNowDate(): String {
         val date = timeHelper.getCurrentDateTime().time
-        val formatter = SimpleDateFormat("EEE, MMM d HH:mm:ss")
+        val formatter = SimpleDateFormat("EEE, MMM d HH:mm:ss", Locale.UK)
         return formatter.format(date)
     }
 
