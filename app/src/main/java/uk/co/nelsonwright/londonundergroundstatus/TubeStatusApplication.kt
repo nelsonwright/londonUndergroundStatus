69d635eb0804a53f1825d6f2e88d72e0f4e5bc51
Nelson Wright <me@nelsonwright.co.uk>
2020-11-17 15:17:31 +0000

    Add comment that activity tests will change to fragment tests

--- app/src/main/java/uk/co/nelsonwright/londonundergroundstatus/TubeStatusApplication.kt
+++ app/src/main/java/uk/co/nelsonwright/londonundergroundstatus/TubeStatusApplication.kt
@@ -17,4 +17,8 @@ class TubeStatusApplication : Application() {
         super.onCreate()
         component = initDagger(this)
     }
+
+    fun setAppComponent(appComponent: AppComponent) {
+        component = appComponent
+    }
 }
