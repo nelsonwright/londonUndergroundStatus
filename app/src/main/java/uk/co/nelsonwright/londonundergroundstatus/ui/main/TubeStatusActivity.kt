883bc0eb5b8f2a72def708f31f5729951459f866
Nelson Wright <me@nelsonwright.co.uk>
2021-01-19 12:07:16 +0000

    Use view binding

--- app/src/main/java/uk/co/nelsonwright/londonundergroundstatus/ui/main/TubeStatusActivity.kt
+++ app/src/main/java/uk/co/nelsonwright/londonundergroundstatus/ui/main/TubeStatusActivity.kt
@@ -2,31 +2,30 @@ package uk.co.nelsonwright.londonundergroundstatus.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import uk.co.nelsonwright.londonundergroundstatus.R
-
+import uk.co.nelsonwright.londonundergroundstatus.databinding.TubeStatusActivityBinding
 
 class TubeStatusActivity : AppCompatActivity() {
     private val fragmentFactory = getFragmentFactory()
-
     private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment }
     private val navController get() = navHostFragment.navController
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: TubeStatusActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportFragmentManager.fragmentFactory = fragmentFactory
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tube_status_activity)
+        binding = TubeStatusActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp()
                 || super.onSupportNavigateUp()
 
